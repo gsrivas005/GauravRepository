a909e4ec4b0dbc476970e5a7375483fdf35499e2
Gaurav <g.srivas005@gmail.com>
2019-10-24 11:46:20 -0700

    Vs File upload

---
+++ GitPractice/SampleCode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Git Uploaded Code");
+        }
+    }
+}
